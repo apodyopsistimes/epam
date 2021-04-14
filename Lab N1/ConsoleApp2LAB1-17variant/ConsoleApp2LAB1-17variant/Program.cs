4dda0ce18072abbbea62d8a9bf07403a5f0c7c83
apodyopsistimes <81700414+apodyopsistimes@users.noreply.github.com>
2021-04-14 17:56:49 +0300

    Add files via upload

---
+++ Lab N1/ConsoleApp2LAB1-17variant/ConsoleApp2LAB1-17variant/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2LAB1_17variant
+{
+    class Program
+    {
+        static decimal Fact (decimal value)
+        {
+            return (value == 0) ? 1 : value * Fact(value - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите значение k");
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Укажите значение x");
+            double x = double.Parse(Console.ReadLine());
+            double result = 0;
+
+            for (int n = 0; n <= k; n++)
+                
+                result += Math.Pow(-1, n) * Math.Pow(x, (4 * n) + 3) / ((2 * n) + 1) * ((4 * n) + 3);
+
+
+            Console.WriteLine("Результат = {0}", result.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab N1/ConsoleApp2LAB1-17variant/LabN1VariantN17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabN1VariantN17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ans = Calculate(0.000001d, 64, 4);
+            Console.WriteLine($"{ans}");
+            Console.ReadLine();
+        }
+
+        static double Calculate(double precision, double a, int n)
+        {
+            double xk = a / 3d;
+            double xk1;
+            do
+            {
+                xk1 = 1d / n * ((n - 1) * xk + a / Math.Pow(xk, n - 1));
+                xk = xk1;
+            }
+            while (Math.Abs(Math.Pow(xk1, n) - a) > precision);
+            return xk1;
+        }
+
+    }
+}
