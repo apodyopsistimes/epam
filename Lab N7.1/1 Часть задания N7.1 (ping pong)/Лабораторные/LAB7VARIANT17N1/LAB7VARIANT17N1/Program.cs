c62b4b6d6ced181b3056ae1e27260577223e1ae2
apodyopsistimes <81700414+apodyopsistimes@users.noreply.github.com>
2021-04-14 18:38:44 +0300

    Add files via upload

---
+++ Lab N7.1/1 Часть задания N7.1 (ping pong)/Лабораторные/LAB7VARIANT17N1/LAB7VARIANT17N1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Action<Func<int, int>, List<string>> list = L;
+        Func<int, int> dvainta = x => x*x;
+        List<string> listik = new List<string>();
+        list?.Invoke(dvainta, listik);
+        foreach (string hi in listik)
+        {
+            Console.WriteLine(hi);
+        }
+        Console.ReadKey();
+
+
+    }
+
+    public static void L(Func<int, int> f, List<string> s)
+    {
+        Random rnd = new Random();
+        int ab = rnd.Next(1, 10);
+        for ( int i = 0; i < ab; i++ )
+            {
+                s.Add(Convert.ToString(f(i)));
+            }
+    }
+
+}
