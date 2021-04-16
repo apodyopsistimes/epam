e7da436669a23a8b39df85d7c23b6810d7545955
apodyopsistimes <81700414+apodyopsistimes@users.noreply.github.com>
2021-04-16 18:36:33 +0300

    Add files via upload

---
+++ Lab N9/Лабораторная/Lab N9.1/Lab N9.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Lab_N9._1
+{
+    [Serializable]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xdoc = new XDocument();
+            XElement root = new XElement("root");
+
+            StreamReader fileTxt = new StreamReader("textfile.txt");
+            string s;
+            int[] number = { 0 , 0};
+
+            
+            while ((s = fileTxt.ReadLine()) != null)
+            {
+                XElement line = new XElement("line");
+                XAttribute num1 = new XAttribute("num", number[0].ToString());
+                line.Add(num1);
+
+                string[] s1 = s.Split();
+                number[1] = 0;
+
+                foreach (string st in s1)
+                {
+                    XElement word = new XElement("word");
+                    XAttribute num2 = new XAttribute("num", number[1].ToString());
+                    XText text = new XText(st);
+                    word.Add(num2);
+                    word.Add(text);
+                    line.Add(word);
+                    number[1]++;
+                }
+                root.Add(line);
+                number[0]++;
+            }
+            fileTxt.Close();
+            xdoc.Add(root);
+            xdoc.Save("xmlLesson1");
+            Console.WriteLine(xdoc + "\n");
+            Console.ReadLine();
+             
+        }
+
+    }
+}
---
+++ Lab N9/Лабораторная/Lab N9.2/Lab N9.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Lab_N9._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xdoc = new XDocument();
+            XElement root = new XElement("root");
+
+            StreamReader fileTxt = new StreamReader("textfile2.txt");
+            string s;
+            while ((s = fileTxt.ReadLine()) != null)
+            {
+                XElement line = new XElement("line"); 
+                string[] s1 = s.Split();
+
+                foreach (string st in s1)
+                {     
+                    XElement word = new XElement("word");
+                    char[] s2 = st.ToCharArray();
+
+                    foreach (char s3 in s2 )
+                    {
+                        XElement chars = new XElement("char");
+                        XText text = new XText(s3.ToString());
+                        chars.Add(text);
+                        word.Add(chars);
+
+                    }
+                    line.Add(word);                 
+                }                             
+                root.Add(line);  
+            }
+
+            fileTxt.Close();
+            xdoc.Add(root);
+            xdoc.Save("xmlLesson2");
+            Console.WriteLine(xdoc);
+            Console.ReadLine();
+        }
+    }
+}
